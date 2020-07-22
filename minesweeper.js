9a475093ef9e0f367cca761fbc7a73eda2152793
rafael <rafaelnhster@gmail.com>
2020-07-22 12:31:54 +1200

    game finish and working

--- minesweeper.js
+++ minesweeper.js
@@ -3,32 +3,33 @@ document.addEventListener('DOMContentLoaded', startGame)
 // Define your `board` object here!
 var board = {
   cells: [
-    { row: 0, col: 0, isMine: false, hidden: true, isMarked: true},
-    { row: 0, col: 1, isMine: false, hidden: true, isMarked: true},
-    { row: 0, col: 2, isMine: true, hidden: true, isMarked: true},
-    { row: 0, col: 3, isMine: false, hidden: true, isMarked: true},
+    { row: 0, col: 0, isMine: false, hidden: true, isMarked: true },
+    { row: 0, col: 1, isMine: false, hidden: true, isMarked: true },
+    { row: 0, col: 2, isMine: true, hidden: true, isMarked: true },
+    { row: 0, col: 3, isMine: false, hidden: true, isMarked: true },
 
-    { row: 1, col: 0, isMine: false, hidden: true, isMarked: true},
-    { row: 1, col: 1, isMine: true, hidden: true, isMarked: true},
-    { row: 1, col: 2, isMine: false, hidden: true, isMarked: true},
-    { row: 1, col: 3, isMine: true, hidden: true, isMarked: true},
+    { row: 1, col: 0, isMine: false, hidden: true, isMarked: true },
+    { row: 1, col: 1, isMine: true, hidden: true, isMarked: true },
+    { row: 1, col: 2, isMine: false, hidden: true, isMarked: true },
+    { row: 1, col: 3, isMine: true, hidden: true, isMarked: true },
 
-    { row: 2, col: 0, isMine: false, hidden: true, isMarked: true},
-    { row: 2, col: 1, isMine: false, hidden: true, isMarked: true},
-    { row: 2, col: 2, isMine: true, hidden: true, isMarked: true},
-    { row: 2, col: 3, isMine: false, hidden: true, isMarked: true},
+    { row: 2, col: 0, isMine: false, hidden: true, isMarked: true },
+    { row: 2, col: 1, isMine: false, hidden: true, isMarked: true },
+    { row: 2, col: 2, isMine: true, hidden: true, isMarked: true },
+    { row: 2, col: 3, isMine: false, hidden: true, isMarked: true },
 
-    { row: 3, col: 0, isMine: false, hidden: true, isMarked: true},
-    { row: 3, col: 1, isMine: true, hidden: true, isMarked: true},
-    { row: 3, col: 2, isMine: false, hidden: true, isMarked: true},
-    { row: 3, col: 3, isMine: false, hidden: true, isMarked: true},
+    { row: 3, col: 0, isMine: false, hidden: true, isMarked: true },
+    { row: 3, col: 1, isMine: true, hidden: true, isMarked: true },
+    { row: 3, col: 2, isMine: false, hidden: true, isMarked: true },
+    { row: 3, col: 3, isMine: false, hidden: true, isMarked: true },
   ]
 }
 
 function startGame() {
-
+  document.addEventListener("click", checkForWin)
+  document.addEventListener("contextmenu", checkForWin)
   for (i = 0; i < board.cells.length; i++) {
-   board.cells[i].surroundingMines = countSurroundingMines(board.cells[i]);
+    board.cells[i].surroundingMines = countSurroundingMines(board.cells[i]);
   }
   // Don't remove this function call: it makes the game work!
   lib.initBoard()
@@ -39,7 +40,14 @@ function startGame() {
 // 1. Are all of the cells that are NOT mines visible?
 // 2. Are all of the mines marked?
 function checkForWin() {
-
+  for (i = 0; i < board.cells.length; i++) {
+    if (board.cells[i].isMine === true && board.cells[i].isMarked === true) {
+      return
+    }
+    else if (board.cells[i].isMarked === true && board.cells[i].hidden === true) {
+      return
+    }
+  }
   // You can use this function call to declare a winner (once you've
   // detected that they've won, that is!)
   lib.displayMessage('You win!')
@@ -55,12 +63,12 @@ function checkForWin() {
 // them, counting the number of times `cell.isMine` is true.
 function countSurroundingMines(cell) {
   var surrounding = lib.getSurroundingCells(cell.row, cell.col)
-  let count = 0; 
-  for (i = 0; i<surrounding.length; i++) {
-    if (surrounding[i].isMine === true){
+  let count = 0;
+  for (var i = 0; i< surrounding.length; i++) {
+    if (surrounding[i].isMine === true) {
       count++;
     }
-   }
-   return count
+  }
+  return count
 }
 
